0f19be090d4391f34601ec124507134ca95ab0e1
Evan Walsh <evan@massivedanger.com>
2013-05-02 13:35:50 -0400

    First commit

---
+++ Massive/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Massive
+//
+//  Created by Evan Walsh on 2/5/13.
+//  Copyright (c) 2013 Massive Danger. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
