ca5d7297597e20ab86390c41ba0da4ca1eae1535
Evan Walsh <evan@massivedanger.com>
2013-06-02 09:19:43 -0400

    Write docs for Common.h

--- Massive/Utilities/Common.h
+++ Massive/Utilities/Common.h
@@ -15,14 +15,16 @@
 #include <vector>
 #include <sstream>
 
-typedef std::string			String;
-typedef std::set<String>	StringSet;
+typedef std::string			String; //! Shorthand for std::string. Simple as that.
+typedef std::set<String>	StringSet; //! Shorthand for a Set of Strings. What did you expect?
 
 #define M Utility
 
+//! Class used to store little functions that are useful everywhere
 class Utility {
     
 public:
+    //! Get String representation of a float
     static String floatToString(float number) {
         std::ostringstream stream;
         stream << number;
